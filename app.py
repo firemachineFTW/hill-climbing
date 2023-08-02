d82e479907d1c8c419c2828e9788e24d5b7e8f1c
firemachineFTW <joanchito-14@live.com>
2023-08-02 07:04:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def index():
     return render_template('index.html', ciudades = ciudades, distancia_total = distancia_total)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = False, host = '0.0.0.0')
